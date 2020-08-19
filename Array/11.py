5769e8b6fefc1848dacfd30d3a0124a5e725c12f
yinchong <yinchong@jd.com>
2020-08-19 08:35:39 +0800

    数组Array: 11. 盛最多水的容器（中等）

---
+++ Array/11.py
@@ -0,0 +1,29 @@
+"""
+11. 盛最多水的容器（中等）
+https://leetcode-cn.com/problems/container-with-most-water/
+
+给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+说明：你不能倾斜容器，且 n 的值至少为 2。
+
+示例：
+
+输入：[1,8,6,2,5,4,8,3,7]
+输出：49
+
+"""
+
+class Solution:
+    # 双指针，依次缩减区间，并计算每个区间构成的矩形的面积
+    # 执行用时：84 ms, 在所有 Python3 提交中击败了21.41%的用户
+    # 内存消耗：15.1 MB, 在所有 Python3 提交中击败了60.27%的用户
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        maxArea = 0
+        while left < right:
+            maxArea = max(maxArea, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return maxArea
