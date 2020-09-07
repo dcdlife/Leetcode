db0ae87dc6901c67efb88bd891743491a613421f
yinchong <yinchong@jd.com>
2020-09-07 21:01:30 +0800

    数组Arrya: 42. 接雨水 (困难)

--- Array/42.py
+++ Array/42.py
@@ -1,3 +1,7 @@
+'''
+Author: yinchong
+Date: 2020-09-02
+'''
 """
 42. 接雨水 (困难)
 https://leetcode-cn.com/problems/trapping-rain-water/
@@ -15,32 +19,90 @@ https://leetcode-cn.com/problems/trapping-rain-water/
 
 """
 
+from typing import List
+
 class Solution:
 
-    # 暴力优化1
-    # 执行用时：900 ms, 在所有 Python3 提交中击败了5.19%的用户
-    # 内存消耗：14 MB, 在所有 Python3 提交中击败了83.98%的用户
+    # 双指针比较难想：
+    def trap(self, height: List[int]) -> int:
+        # 边界条件
+        if not height: return 0
+        n = len(height)
+
+        left,right = 0, n - 1  # 分别位于输入数组的两端
+        maxleft,maxright = height[0],height[n - 1]
+        ans = 0
+
+        while left <= right:
+            maxleft = max(height[left],maxleft)
+            maxright = max(height[right],maxright)
+            if maxleft < maxright:
+                ans += maxleft - height[left]
+                left += 1
+            else:
+                ans += maxright - height[right]
+                right -= 1
+
+        return ans
+
+    # 栈的应用 时间O(n) 空间O(n)
+    # 执行用时：52 ms, 在所有 Python3 提交中击败了56.60%的用户
+    # 内存消耗：14.2 MB, 在所有 Python3 提交中击败了30.31%的用户
+    def trap3(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        length = len(height) 
+        if length < 3:
+            return 0
+
+        ans = 0
+        current = 0
+        stack = []
+        while current < length:
+            while len(stack) and height[current] > height[stack[-1]]:
+                top = stack.pop()
+                if len(stack) == 0:
+                    break
+                h = min(height[stack[-1]], height[current]) - height[top]
+                distance = current - stack[-1] - 1
+                ans += h * distance
+            stack.append(current)
+            current += 1
+
+        return ans
+
+    # 暴力优化之动态编程: 时间O(n) 空间O(n)。
+    # 先通过两次for循环分别找出索引左侧的最大值和右侧的最大值
+    # 执行用时：44 ms, 在所有 Python3 提交中击败了89.34%的用户
+    # 内存消耗：14.2 MB, 在所有 Python3 提交中击败了22.68%的用户
+    # 注意: [2,0,2]
     def trap2(self, height: List[int]) -> int:
         if not height:
             return 0
 
-        length = len(height)   
-
+        length = len(height) 
         if length < 3:
             return 0
-        ans = 0
-        max_left = height[0]
-        for i in range(1, length - 1):
-            max_left = max(max_left, height[i-1])
-            max_right = max(height[i+1:])
-            minInMaxHeights = min(max_left, max_right)
-            if height[i] < minInMaxHeights:
-                ans += minInMaxHeights - height[i]
 
+        max_lefts = [0] * length
+        max_lefts[0] = height[0]
+        for i in range(1, length):
+            max_lefts[i] = max(height[i], max_lefts[i - 1])
+
+        max_rights = [0] * length
+        max_rights[length - 1] = height[length - 1]
+        for i in range(length - 2, -1, -1):
+            max_rights[i] = max(height[i], max_rights[i + 1])
+        
+        ans = 0
+        for i in range(1, length - 1):
+            ans += max(0, min(max_lefts[i], max_rights[i]) - height[i])
+        
         return ans
 
 
-    # 暴力法: 
+    # 暴力法 时间O(n^2) 空间O(1): 
     # 执行用时：1712 ms, 在所有 Python3 提交中击败了5.01%的用户
     # 内存消耗：14.3 MB, 在所有 Python3 提交中击败了6.04%的用户
     def trap1(self, height: List[int]) -> int:
@@ -56,3 +118,9 @@ class Solution:
                 ans += minInMaxHeights - height[i]
 
         return ans
+
+
+if __name__ == "__main__":
+    su = Solution()
+    print(su.trap([2,0,2]))
+    print(su.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
