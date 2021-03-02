a3b3702aea5e50a587bf4442e533b1fd7cf7beb0
yc995 <836058144@qq.com>
2021-03-03 06:07:06 +0800

    添加数组实现

--- TXKT/TXKT/TXKT/ArrayList.swift
+++ TXKT/TXKT/TXKT/ArrayList.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+/*
+ ◼ int size(); // 元素的数量
+ ◼ boolean isEmpty(); // 是否为空
+ ◼ boolean contains(E element); // 是否包含某个元素
+ ◼ void add(E element); // 添加元素到最后面
+ ◼ void add(int index, E element) // 在index位置插入一个元素
+ ◼ E get(int index); // 返回index位置对应的元素
+ ◼ E set(int index, E element); // 设置index位置的元素
+ ◼ void add(int index, E element); // 往index位置添加元素
+ ◼ E remove(int index); // 删除index位置对应的元素
+ ◼ int indexOf(E element); // 查看元素的位置
+ ◼ void clear(); // 清除所有元素
+ */
+
 class ArrayList {
     /// 元素数量
     var itemSize: Int
@@ -17,8 +31,8 @@ class ArrayList {
     let ELEMENT_NOT_FOUND: Int = -1
     
     init(capacity: Int = 10) {
-        itemSize = (capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capacity
-        elements = Array(repeating: 0, count: itemSize)
+        itemSize = 0
+        elements = Array(repeating: 0, count: (capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capacity)
     }
     
     /// 元素的数量
@@ -26,7 +40,120 @@ class ArrayList {
         return self.itemSize
     }
     
+    /// 是否为空
     func isEmpty() -> Bool {
-        return false
+        return itemSize == 0
+    }
+    
+    /// 是否包含某个元素
+    func contains(_ e: Int) -> Bool {
+        return indexOf(e) != ELEMENT_NOT_FOUND
+    }
+    
+    /// 添加元素到最后面
+    func add(_ e: Int) {
+        add(itemSize, e)
+    }
+    
+    /// 在index位置插入一个元素
+    func add(_ index: Int, _ e: Int) {
+        if !rangeCheckForAdd(index) {
+            return
+        }
+        ensureCapacity(itemSize + 1)
+        for i in stride(from:itemSize, through:index + 1, by: -1) {
+            elements[i] =  elements[i - 1]
+        }
+        elements[index] = e
+        itemSize += 1
+    }
+    
+    /// 返回index位置对应的元素
+    func get(_ index: Int) -> Int? {
+        if !rangeCheck(index) {
+            return nil
+        }
+        return elements[index]
+    }
+    
+    /// 设置index位置的元素
+    func set(_ index: Int, _ e: Int) -> Int? {
+        if !rangeCheck(index) {
+            return nil
+        }
+        let old = elements[index]
+        elements[index] = e
+        return old
+    }
+    
+    /// 删除index位置对应的元素
+    func remove(_ index: Int) -> Int? {
+        if !rangeCheck(index) {
+            return nil
+        }
+        let old = elements[index]
+        for i in (index + 1)..<itemSize {
+            elements[i - 1] = elements[i]
+        }
+        itemSize -= 1
+        return old
+    }
+    
+    /// 查看元素的位置
+    func indexOf(_ e: Int) -> Int {
+        for i in 0..<elements.count {
+            if elements[i] == e {
+                return i
+            }
+        }
+        return ELEMENT_NOT_FOUND
+    }
+    
+    /// 清除所有的元素
+    func clear() {
+        /// 如果元素是对象，需要打开下面的代码
+//        for i in 0..<itemSize {
+//            elements[i] = nil
+//        }
+        itemSize = 0
+    }
+    
+    func rangeCheck(_ index: Int) -> Bool {
+        if (index < 0 || index >= itemSize) {
+            if itemSize == 0 {
+                print("当前没有任何元素")
+            } else {
+                print("越界了，范围应该是[0,\(itemSize - 1)]")
+            }
+            return false
+        }
+        return true
+    }
+    
+    func rangeCheckForAdd(_ index: Int) -> Bool {
+        if (index < 0 || index > itemSize) {
+            print("\(index)越界了，范围应该是[0,\(itemSize)]")
+//            enum ArrayListError: Error {
+//                case outOfBoundss(Int, Int)
+//            }
+//            throw ArrayListError.outOfBoundss(index, itemSize)
+            return false
+        }
+        return true
+    }
+    
+    func ensureCapacity(_ capacity: Int) {
+        let curCapacity = elements.count
+        if curCapacity >= capacity {
+            return
+        }
+        // 新容量为旧容量的1.5倍
+        let newCapacity = curCapacity + curCapacity >> 1
+        var newElements: [Int] = Array(repeating: 0, count: newCapacity)
+        for i in 0..<itemSize {
+            newElements[i] = elements[i]
+        }
+        elements = newElements
+        print("ArrayList：从\(curCapacity)扩容到\(newCapacity)")
     }
 }
--- TXKT/TXKT/TXKT/main.swift
+++ TXKT/TXKT/TXKT/main.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
-let list = ArrayList()
+let list = ArrayList(capacity: 5)
 print(list.elements)
-
+print(list.size())
+print(list.isEmpty())
+list.add(10)
+list.add(11)
+print(list.elements)
+print(list.size())
+print(list.isEmpty())
+print(list.contains(10), list.contains(11), list.contains(12))
+list.add(0, 999)
+print(list.elements, list.size())
+print(list.get(0)!, list.get(1)!, list.get(2)!)
+print(list.set(0, 999999999)!)
+print(list.get(0)!, list.get(1)!, list.get(2)!)
+print(list.indexOf(999999999), list.indexOf(10), list.indexOf(11))
+print(list.elements)
+list.remove(0)
+print(list.elements, list.size())
+print(list.get(2))
+list.clear()
+print(list.elements, list.size())
+print(list.get(2))
