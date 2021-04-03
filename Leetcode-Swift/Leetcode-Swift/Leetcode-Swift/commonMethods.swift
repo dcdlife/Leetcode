2d23acbdd7e387a0752b4340424e2d82c51c2f3f
yc995 <836058144@qq.com>
2021-04-03 16:03:13 +0800

    142. 环形链表 II (中等)

--- Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/commonMethods.swift
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/commonMethods.swift
@@ -25,6 +25,29 @@ func createListNode(values: [Int]) -> ListNode? {
     return resNode
 }
 
+func createCycleListNode(_ values: [Int], pos: Int) -> ListNode? {
+    if values.count != 0 && pos != -1 {
+        let head: ListNode? = createListNode(values: values)
+        
+        var endNode: ListNode? = head
+        while endNode?.next != nil {
+            endNode = endNode?.next
+        }
+        
+        var entranceNode: ListNode? = head
+        var index = 0
+        while index != pos {
+            index += 1
+            entranceNode = entranceNode?.next
+        }
+        
+        endNode?.next = entranceNode
+        return head
+    } else {
+        return createListNode(values: values)
+    }
+}
+
 /// 依次打印链表节点
 func printListNode(rootNode: ListNode?) {
     if rootNode == nil {
--- Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/main.swift
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/main.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 
-let su = Solution24()
+let su = Solution142()
 su.test()
