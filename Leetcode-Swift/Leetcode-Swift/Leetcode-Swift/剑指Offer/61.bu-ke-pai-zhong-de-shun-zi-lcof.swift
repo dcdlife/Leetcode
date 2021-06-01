9136f15fd17bfb785f51e77040f226f74e908474
devyc95 <71340227+devyc95@users.noreply.github.com>
2021-06-02 07:01:01 +0800

    剑指 Offer 61. 扑克牌中的顺子 (简单)

---
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/剑指Offer/61.bu-ke-pai-zhong-de-shun-zi-lcof.swift
@@ -0,0 +1,63 @@
+//
+//  61.bu-ke-pai-zhong-de-shun-zi-lcof.swift
+//  Leetcode-Swift
+//
+//  Created by devyc on 2021/6/2.
+//
+
+import Foundation
+
+/*
+ 剑指 Offer 61. 扑克牌中的顺子 (简单)
+ https://leetcode-cn.com/problems/bu-ke-pai-zhong-de-shun-zi-lcof/
+ */
+
+class Solution_Offer_61 {
+    func isStraight(_ nums: [Int]) -> Bool {
+        if nums.count == 0 {
+            return true
+        }
+        
+        let count = nums.count
+        let nums = nums.sorted(by: <)
+        
+        var zeroCount = 0
+        var index = 0
+        while index < count - 1 && nums[index] == 0 {
+            zeroCount += 1
+            index += 1
+        }
+        
+        var numberOfGap = 0
+        var next = index + 1
+        while next < count {
+            let pre = next - 1
+            if nums[next] == nums[pre] {
+                return false
+            }
+            
+            numberOfGap += (nums[next] - nums[pre] - 1)
+            next += 1
+        }
+        
+        return numberOfGap > zeroCount ? false : true
+    }
+    
+    /*
+     测试用例：
+     1. 功能：不含0的顺子和非顺子；含0的顺子和非顺子
+     2. 特殊：空数组；
+     */
+    func test() {
+        let tests = [
+            [1,2,3,4,5],
+            [1,10,2,3,4],
+            [1,2,5,0,0],
+            [1,2,5,0,10],
+            []
+        ]
+        for i in tests {
+            print(isStraight(i))
+        }
+    }
+}
