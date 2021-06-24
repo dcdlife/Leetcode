7eacfa4586569e0746eaf948f7c1ec6cccadb6d1
devyc95 <71340227+devyc95@users.noreply.github.com>
2021-06-25 07:34:18 +0800

    705. 设计哈希集合 (简单)

---
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/leetcode/linked-list/705.design-hashset.swift
@@ -0,0 +1,70 @@
+//
+//  705.design-hashset.swift
+//  Leetcode-Swift
+//
+//  Created by devyc on 2021/6/25.
+//
+
+import Foundation
+
+/*
+ 705. 设计哈希集合 (简单)
+ https://leetcode-cn.com/problems/design-hashset/
+ */
+
+class Solution_705 {
+    class MyHashSet {
+
+        var base: Int
+        var data: [[Int]]
+        /** Initialize your data structure here. */
+        init() {
+            base = 100
+            data = [[Int]](repeating: [Int](), count: base)
+        }
+        
+        func add(_ key: Int) {
+            let index = hash(key)
+            let values = data[index]
+            for i in values {
+                if i == key {
+                    return
+                }
+            }
+            data[index].append(key)
+        }
+        
+        func remove(_ key: Int) {
+            let keyIndex = hash(key)
+            for (index, i) in data[keyIndex].enumerated() {
+                if i == key {
+                    data[keyIndex].remove(at: index)
+                    return
+                }
+            }
+        }
+        
+        /** Returns true if this set contains the specified element */
+        func contains(_ key: Int) -> Bool {
+            for i in data[hash(key)] {
+                if i == key {
+                    return true
+                }
+            }
+            return false
+        }
+        
+        func hash(_ key: Int) -> Int {
+            return key % base
+        }
+    }
+    
+    func test() {
+        let obj = MyHashSet()
+        obj.add(1)
+        obj.add(2)
+        print(obj.contains(1))
+        obj.remove(1)
+        print(obj.contains(1))
+    }
+}
--- Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/main.swift
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/main.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 
-let su = Solution_1019()
+let su = Solution_705()
 su.test()
