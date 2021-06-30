600ab16edb1fa7956bef7cb4aa24684779334fbc
devyc95 <71340227+devyc95@users.noreply.github.com>
2021-07-01 07:04:53 +0800

    面试题 02.07. 链表相交 (简单)

--- Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/main.swift
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/main.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 
-let su = Solution_355()
+let su = Solution_432()
 su.test()
---
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/程序员面试金典(第6版)/02.07.intersection-of-two-linked-lists-lcci.swift
@@ -0,0 +1,60 @@
+//
+//  02.07.intersection-of-two-linked-lists-lcci.swift
+//  Leetcode-Swift
+//
+//  Created by devyc on 2021/7/1.
+//
+
+import Foundation
+
+/*
+ 面试题 02.07. 链表相交 (简单)
+ https://leetcode-cn.com/problems/intersection-of-two-linked-lists-lcci/
+ */
+
+
+/// JavaScript
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+//var getIntersectionNode = function(headA, headB) {
+//    if (headA == null || headB == null) {
+//        return null
+//    }
+//
+//    let stackA = []
+//    let stackB = []
+//
+//    let node = headA
+//    while (node != null) {
+//        stackA.push(node)
+//        node = node.next
+//    }
+//    node = headB
+//    while (node != null) {
+//        stackB.push(node)
+//        node = node.next
+//    }
+//
+//    let ans = null
+//    while (stackA.length && stackB.length) {
+//        if (stackA[stackA.length - 1] == stackB[stackB.length - 1]) {
+//            ans = stackA.pop()
+//            stackB.pop()
+//        } else {
+//            break
+//        }
+//    }
+//
+//    return ans
+//};
