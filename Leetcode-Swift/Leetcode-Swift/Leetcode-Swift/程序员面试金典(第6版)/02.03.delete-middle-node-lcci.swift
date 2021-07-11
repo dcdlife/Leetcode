e18f038af81d4844e602059ffca1416aa2ece324
devyc95 <71340227+devyc95@users.noreply.github.com>
2021-07-12 06:53:54 +0800

    面试题 02.03. 删除中间节点 (简单)

---
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/程序员面试金典(第6版)/02.03.delete-middle-node-lcci.swift
@@ -0,0 +1,36 @@
+//
+//  02.03.delete-middle-node-lcci.swift
+//  Leetcode-Swift
+//
+//  Created by devyc on 2021/7/12.
+//
+
+import Foundation
+
+/*
+ 面试题 02.03. 删除中间节点 (简单)
+ https://leetcode-cn.com/problems/delete-middle-node-lcci/
+ */
+
+/* JavaScript:
+ 
+ /**
+  * Definition for singly-linked list.
+  * function ListNode(val) {
+  *     this.val = val;
+  *     this.next = null;
+  * }
+  */
+ /**
+  * @param {ListNode} node
+  * @return {void} Do not return anything, modify node in-place instead.
+  */
+ var deleteNode = function(node) {
+     if (node == null || node.next == null) {
+         return
+     }
+     node.val = node.next.val
+     node.next = node.next.next
+ };
+ 
+ */
