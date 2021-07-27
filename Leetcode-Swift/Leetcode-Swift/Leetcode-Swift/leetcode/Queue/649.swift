f40eec924ad09b8148a7b09c1cd7afda75edf0d9
devyc95 <71340227+devyc95@users.noreply.github.com>
2021-07-28 06:38:37 +0800

    649. Dota2 参议院 (中等)

---
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/leetcode/Queue/649.swift
@@ -0,0 +1,50 @@
+//
+//  649.swift
+//  Leetcode-Swift
+//
+//  Created by devyc on 2021/7/28.
+//
+
+import Foundation
+
+/*
+ 649. Dota2 参议院 (中等)
+ https://leetcode-cn.com/problems/dota2-senate/
+ */
+
+class Solution_649 {
+    func predictPartyVictory(_ senate: String) -> String {
+        if senate.isEmpty {
+            return ""
+        }
+        
+        var queueR = [Int]()
+        var queueD = [Int]()
+        for (index, value) in senate.enumerated() {
+            if value == "R" {
+                queueR.append(index)
+            } else {
+                queueD.append(index)
+            }
+        }
+        
+        let allMember = senate.count
+        while !queueR.isEmpty && !queueD.isEmpty {
+            let headR = queueR.removeFirst()
+            let headD = queueD.removeFirst()
+            if headR < headD {
+                queueR.append(headR + allMember)
+            } else {
+                queueD.append(headD + allMember)
+            }
+        }
+        
+        return queueD.isEmpty ? "Radiant" : "Dire"
+    }
+    
+    func test() {
+        print(predictPartyVictory("RD"))
+        print(predictPartyVictory("RDD"))
+        print(predictPartyVictory("RRD"))
+    }
+}
--- Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/main.swift
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/main.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 
-let su = Solution_387()
+let su = Solution_649()
 su.test()
