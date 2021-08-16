94b26574d95e782c581ddf7bc4aa541d3cd34039
devyc95 <71340227+devyc95@users.noreply.github.com>
2021-08-17 07:52:16 +0800

    20天「算法」刷题计划 - Day10： 1. [21. 合并两个有序链表 (简单)](https://leetcode-cn.com/problems/merge-two-sorted-lists/) 2. [206. 反转链表 (简单)](https://leetcode-cn.com/problems/reverse-linked-list/)

---
+++ Leetcode-Swift/Leetcode-Swift/Leetcode-Swift/leetcode/LeetCode官方 - 学习计划/20天「算法」刷题计划/206_Day20Plan.swift
@@ -0,0 +1,47 @@
+//
+//  206_Day20Plan.swift
+//  Leetcode-Swift
+//
+//  Created by devyc on 2021/8/17.
+//
+
+import Foundation
+
+/*
+ 206. 反转链表 (简单)
+ https://leetcode-cn.com/problems/reverse-linked-list/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution_206_Day20Plan {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        if head == nil {
+            return nil
+        }
+        
+        var pre: ListNode? = nil, cur = head
+        while cur != nil {
+            let next = cur?.next
+            cur?.next = pre
+            pre = cur
+            cur = next
+        }
+        
+        return pre
+    }
+    
+    func test() {
+        printListNode(rootNode: reverseList(createListNode(values: [1,2,3])))
+        printListNode(rootNode: reverseList(createListNode(values: [1,2])))
+        printListNode(rootNode: reverseList(createListNode(values: [1])))
+    }
+}
