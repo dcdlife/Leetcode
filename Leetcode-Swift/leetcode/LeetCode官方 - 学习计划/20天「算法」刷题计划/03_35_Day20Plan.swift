d9dad78175da663f61d8cef0d623b8cf5cbe3438
devshiye <71340227+devshiye@users.noreply.github.com>
2021-08-21 22:43:13 +0800

    更新20天「算法」刷题计划题目：35

--- Leetcode-Swift/leetcode/LeetCode官方 - 学习计划/20天「算法」刷题计划/03_35_Day20Plan.swift
+++ Leetcode-Swift/leetcode/LeetCode官方 - 学习计划/20天「算法」刷题计划/03_35_Day20Plan.swift
@@ -21,24 +21,13 @@ class Solution_35_Day20Plan {
         var left = 0
         var right = nums.count - 1
         
-//        while left <= right {
-//            let mid = left + (right - left) >> 1
-//            let val = nums[mid]
-//            if val == target {
-//                return mid
-//            } else if val > target {
-//                right = mid - 1
-//            } else {
-//                left = mid + 1
-//            }
-//        }
-//        return left
-        
-        var ans = nums.count
+        var ans = nums.count // 默认插入的索引为最后
         while left <= right {
             let mid = left + (right - left) >> 1
             let val = nums[mid]
             if val >= target {
+                // 题目给了一个前提：数组中没有重复元素，这很重要，因此我们只需要找到第一个大于等于target的值的索引即可。
+                // 如果val=target，那么mid刚好就是目标值的索引；如果数组中没有target，那么寻找第一个大于target的值所在的索引，即这个数要插入的索引
                 ans = mid
                 right = mid - 1
             } else {
