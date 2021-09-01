79f1ed668236e5c573323cb3c59e55aed1193092
devshiye <71340227+devshiye@users.noreply.github.com>
2021-09-02 06:34:38 +0800

    20天「算法」刷题计划 - 算法基础 - Day12： 1. [213. 打家劫舍 II (中等)](https://leetcode-cn.com/problems/house-robber-ii/) 2. [55. 跳跃游戏 (中等)](https://leetcode-cn.com/problems/jump-game/)

---
+++ Leetcode-Swift/LeetCode官方学习计划/20天「算法」刷题计划/算法基础/29_213_Day20Plan.swift
@@ -0,0 +1,48 @@
+//
+//  29_213_Day20Plan.swift
+//  Leetcode-Swift
+//
+//  Created by devshiye on 2021/9/2.
+//
+
+import Foundation
+
+/*
+ 213. 打家劫舍 II (中等)
+ https://leetcode-cn.com/problems/house-robber-ii/
+ */
+
+class Solution_29_213_Day20Plan {
+    /// 动态规划（max(去头，去尾)）
+    func rob(_ nums: [Int]) -> Int {
+        func robRange(_ begin: Int, _ end: Int) -> Int {
+            var first = nums[begin]
+            var second = max(nums[begin], nums[begin + 1])
+            var index = begin + 2
+            while index <= end {
+                let newSecond = max(second, nums[index] + first)
+                first = second
+                second = newSecond
+                
+                index += 1
+            }
+            return second
+        }
+        
+        let len = nums.count
+        if len == 1 {
+            return nums[0]
+        } else if len == 2 {
+            return max(nums[0], nums[1])
+        } else {
+            return max(robRange(0, len - 2), robRange(1, len - 1))
+        }
+    }
+    
+    func test() {
+        print(rob([2,3,2]))
+        print(rob([1,2,3,1]))
+        print(rob([0]))
+        print(rob([1,2]))
+    }
+}
---
+++ Leetcode-Swift/LeetCode官方学习计划/20天「算法」刷题计划/算法基础/30_55_Day20Plan.swift
@@ -0,0 +1,40 @@
+//
+//  30_55_Day20Plan.swift
+//  Leetcode-Swift
+//
+//  Created by devshiye on 2021/9/2.
+//
+
+import Foundation
+
+/*
+ 55. 跳跃游戏 (中等)
+ https://leetcode-cn.com/problems/jump-game/
+ */
+
+class Solution_30_55_Day20Plan {
+    /// 贪心算法
+    func canJump(_ nums: [Int]) -> Bool {
+        let len = nums.count
+        var rightmost = 0
+        for i in 0..<len {
+            if i <= rightmost {
+                rightmost = max(rightmost, i + nums[i])
+                if rightmost >= len - 1 {
+                    return true
+                }
+            }
+        }
+        
+        return false
+    }
+    
+    func test() {
+        print(canJump([2,3,1,1,4]))
+        print(canJump([3,2,1,0,4]))
+        print(canJump([0]))
+        print(canJump([0,1]))
+        
+        print(canJump([1,2,3]))
+    }
+}
