a13268692cb4c08c288cb826331f8b06b8b9f88b
devshiye <71340227+devshiye@users.noreply.github.com>
2021-09-26 22:41:44 +0800

    3周攻克「数据结构」- Day5: 1. [36. 有效的数独 (中等)](https://leetcode-cn.com/problems/valid-sudoku/) 2. [73. 矩阵置零 (中等)](https://leetcode-cn.com/problems/set-matrix-zeroes/)

---
+++ Leetcode-Swift/LeetCode官方学习计划/3周攻克「数据结构」/数据结构入门/09_36.swift
@@ -0,0 +1,64 @@
+//
+//  09_36.swift
+//  Leetcode-Swift
+//
+//  Created by devshiye on 2021/9/26.
+//
+
+import Foundation
+
+/*
+ 36. 有效的数独 (中等)
+ https://leetcode-cn.com/problems/valid-sudoku/
+ */
+
+class Solution_36_3weeksDataStructures {
+    func isValidSudoku(_ board: [[Character]]) -> Bool {
+        var rowsSets = [Set<Character>](repeating: Set<Character>(), count: 9)
+        var colsSets = [Set<Character>](repeating: Set<Character>(), count: 9)
+        var squareSets = [Set<Character>](repeating: Set<Character>(), count: 9)
+        for i in 0..<9 {
+            for j in 0..<9 {
+                let value: Character = board[i][j]
+                if value == "." {
+                    continue
+                }
+                if rowsSets[i].contains(value) {
+                    return false
+                }
+                if colsSets[j].contains(value) {
+                    return false
+                }
+                let squreIndex = 3 * (i / 3) + j / 3
+                if squareSets[squreIndex].contains(value) {
+                    return false
+                }
+                rowsSets[i].insert(value)
+                colsSets[j].insert(value)
+                squareSets[squreIndex].insert(value)
+            }
+        }
+        return true
+    }
+    
+    func test() {
+        print(isValidSudoku([["5","3",".",".","7",".",".",".","."]
+                             ,["6",".",".","1","9","5",".",".","."]
+                             ,[".","9","8",".",".",".",".","6","."]
+                             ,["8",".",".",".","6",".",".",".","3"]
+                             ,["4",".",".","8",".","3",".",".","1"]
+                             ,["7",".",".",".","2",".",".",".","6"]
+                             ,[".","6",".",".",".",".","2","8","."]
+                             ,[".",".",".","4","1","9",".",".","5"]
+                             ,[".",".",".",".","8",".",".","7","9"]]))
+        print(isValidSudoku([["8","3",".",".","7",".",".",".","."]
+                             ,["6",".",".","1","9","5",".",".","."]
+                             ,[".","9","8",".",".",".",".","6","."]
+                             ,["8",".",".",".","6",".",".",".","3"]
+                             ,["4",".",".","8",".","3",".",".","1"]
+                             ,["7",".",".",".","2",".",".",".","6"]
+                             ,[".","6",".",".",".",".","2","8","."]
+                             ,[".",".",".","4","1","9",".",".","5"]
+                             ,[".",".",".",".","8",".",".","7","9"]]))
+    }
+}
---
+++ Leetcode-Swift/LeetCode官方学习计划/3周攻克「数据结构」/数据结构入门/10_73.swift
@@ -0,0 +1,71 @@
+//
+//  10_73.swift
+//  Leetcode-Swift
+//
+//  Created by devshiye on 2021/9/26.
+//
+
+import Foundation
+
+/*
+ 73. 矩阵置零 (中等)
+ https://leetcode-cn.com/problems/set-matrix-zeroes/
+ 
+ 推荐题解：
+ 1. https://leetcode-cn.com/problems/set-matrix-zeroes/solution/o1kong-jian-by-powcai/
+ */
+
+class Solution_73_3weeksDataStructures {
+    func setZeroes(_ matrix: inout [[Int]]) {
+        if matrix.isEmpty {
+            return
+        }
+        let m = matrix.count
+        let n = matrix[0].count
+        var row0HasZero = false
+        var col0HasZero = false
+        for i in 0..<n {
+            if matrix[0][i] == 0 {
+                row0HasZero = true
+                break
+            }
+        }
+        for i in 0..<m {
+            if matrix[i][0] == 0 {
+                col0HasZero = true
+                break
+            }
+        }
+        for i in 1..<m {
+            for j in 1..<n {
+                if matrix[i][j] == 0 {
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+                }
+            }
+        }
+        for i in 1..<m {
+            for j in 1..<n {
+                if matrix[i][0] == 0 || matrix[0][j] == 0 {
+                    matrix[i][j] = 0
+                }
+            }
+        }
+        if row0HasZero {
+            for j in 0..<n {
+                matrix[0][j] = 0
+            }
+        }
+        if col0HasZero {
+            for i in 0..<m {
+                matrix[i][0] = 0
+            }
+        }
+    }
+    
+    func test() {
+        var matrix = [[1,1,1],[1,0,1],[1,1,1]]
+        setZeroes(&matrix)
+        matrix.print()
+    }
+}
