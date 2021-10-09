cf734e2177a83aa0aa9ceebf008269f260e13065
devshiye <71340227+devshiye@users.noreply.github.com>
2021-10-09 23:31:13 +0800

    添加：226. 翻转二叉树 (简单)

---
+++ Leetcode-Swift/LeetCode官方学习计划/3周攻克「数据结构」/数据结构入门/27_226.swift
@@ -0,0 +1,20 @@
+//
+//  27_226.swift
+//  Leetcode-Swift
+//
+//  Created by devshiye on 2021/10/9.
+//
+
+import Foundation
+
+/*
+ 226. 翻转二叉树 (简单)
+ https://leetcode-cn.com/problems/invert-binary-tree/
+ */
+
+class Solution_226_3weeksDataStructures {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+
+        return nil
+    }
+}
--- Leetcode-Swift/main.swift
+++ Leetcode-Swift/main.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 
-let su = Solution_101_3weeksDataStructures()
+let su = Solution_226_3weeksDataStructures()
 su.test()
