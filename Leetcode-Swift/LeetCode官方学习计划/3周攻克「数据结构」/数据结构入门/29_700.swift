428ec8c09980dee36a1d0dcbb36f12c16cd50c25
devshiye <71340227+devshiye@users.noreply.github.com>
2021-10-14 07:03:26 +0800

    3周攻克「数据结构」- Day13: 1. 701. 二叉搜索树中的插入操作 (中等)

---
+++ Leetcode-Swift/LeetCode官方学习计划/3周攻克「数据结构」/数据结构入门/29_700.swift
@@ -0,0 +1,42 @@
+//
+//  29_700.swift
+//  Leetcode-Swift
+//
+//  Created by devshiye on 2021/10/14.
+//
+
+import Foundation
+
+/*
+ 700. 二叉搜索树中的搜索 (简单)
+ https://leetcode-cn.com/problems/search-in-a-binary-search-tree/
+ */
+
+class Solution_700_3weeksDataStructures {
+    /// 迭代
+    func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        var root = root
+        while root != nil && root!.val != val {
+            root = root!.val > val ? root?.left : root?.right
+        }
+        return root
+    }
+    
+    /// 递归
+    func searchBST_1(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        if root == nil {
+            return nil
+        }
+        
+        let rootVal = root!.val
+        if rootVal == val {
+            return root
+        }
+        
+        return rootVal > val ? searchBST(root?.left, val) : searchBST(root?.right, val)
+    }
+    
+    func test() {
+        print(searchBST(createTreeNode([4,2,7,1,3,6,8]), 2)?.levelOrder())
+    }
+}
---
+++ Leetcode-Swift/LeetCode官方学习计划/3周攻克「数据结构」/数据结构入门/30_701.swift
@@ -0,0 +1,24 @@
+//
+//  30_701.swift
+//  Leetcode-Swift
+//
+//  Created by devshiye on 2021/10/14.
+//
+
+import Foundation
+
+/*
+ 701. 二叉搜索树中的插入操作 (中等)
+ https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/
+ */
+
+class Solution_701_3weeksDataStructures {
+    func insertIntoBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+
+        return nil
+    }
+    
+    func test() {
+        print("test")
+    }
+}
