9f0698790bc3b18ffe911f5f11b819b92554d2ad
devshiye <71340227+devshiye@users.noreply.github.com>
2021-12-07 22:13:04 +0800

    83. 删除排序链表中的重复元素 (简单)

--- Leetcode-Swift/LeetCode官方学习计划/3周攻克「数据结构」/数据结构入门/18_83.swift
+++ Leetcode-Swift/LeetCode官方学习计划/3周攻克「数据结构」/数据结构入门/18_83.swift
@@ -10,27 +10,7 @@ import Foundation
 /*
  83. 删除排序链表中的重复元素 (简单)
  https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+ 
+ 参考：class Solution_83
  */
 
-class Solution_83_3weeksDataStructures {
-    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
-        var pre = head
-        var cur = pre?.next
-        while cur != nil {
-            let newCur = cur?.next
-            if pre!.val == cur!.val {
-                pre?.next = cur?.next
-            } else {
-                pre = cur
-            }
-            cur = newCur
-        }
-        return head
-    }
-    
-    func test() {
-        printListNode(rootNode: deleteDuplicates(createListNode(values: [1,1,2,3,3])))
-        printListNode(rootNode: deleteDuplicates(createListNode(values: [1,1,2])))
-        printListNode(rootNode: deleteDuplicates(createListNode(values: [1,1])))
-    }
-}
---
+++ Leetcode-Swift/Leetcode/83.swift
@@ -0,0 +1,39 @@
+//
+//  83.swift
+//  Leetcode-Swift
+//
+//  Created by devshiye on 2021/12/7.
+//
+
+import Foundation
+
+/*
+ 83. 删除排序链表中的重复元素 (简单)
+ https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+ */
+
+class Solution_83 {
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        // 参数校验
+        if head == nil {
+            return nil
+        }
+        
+        var cur = head
+        while cur != nil {
+            while let next = cur?.next, cur!.val == next.val {
+                cur?.next = cur?.next?.next
+            }
+            cur = cur?.next
+        }
+        
+        return head
+    }
+    
+    func test() {
+        printListNode(rootNode: deleteDuplicates(createListNode(values: [1,1,2,3,3])))
+        printListNode(rootNode: deleteDuplicates(createListNode(values: [1,1,2])))
+        printListNode(rootNode: deleteDuplicates(createListNode(values: [1,1])))
+    }
+}
+
