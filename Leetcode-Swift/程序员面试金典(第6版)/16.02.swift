053b9a8483877158d6216948e14de2bdf7759005
devyefeng <71340227+devyefeng@users.noreply.github.com>
2022-01-02 19:01:52 +0800

    面试题 16.02. 单词频率 (中等)

---
+++ Leetcode-Swift/程序员面试金典(第6版)/16.02.swift
@@ -0,0 +1,33 @@
+//
+//  16.02.swift
+//  Leetcode-Swift
+//
+//  Created by devshiye on 2022/1/2.
+//
+
+import Foundation
+
+/*
+ 面试题 16.02. 单词频率 (中等)
+ https://leetcode-cn.com/problems/words-frequency-lcci/
+ */
+
+class Solution_Jindian_1602 {
+    class WordsFrequency {
+        
+        var map = [String: Int]()
+        
+        init(_ book: [String]) {
+            for i in book {
+                map[i, default: 0] += 1
+            }
+        }
+        
+        func get(_ word: String) -> Int {
+            if let frequency = map[word] {
+                return frequency
+            }
+            return 0
+        }
+    }
+}
