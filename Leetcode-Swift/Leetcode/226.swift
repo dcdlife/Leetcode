79abcf700047225d266adb74f44e28dab4a15e09
devchongzai <71340227+devchongzai@users.noreply.github.com>
2022-04-11 06:00:09 +0800

    226. 翻转二叉树 (简单)

---
+++ Leetcode-Swift/Leetcode/226.swift
@@ -0,0 +1,89 @@
+//
+//  226.swift
+//  Leetcode-Swift
+//
+//  Created by yefeng on 2022/4/11.
+//
+
+import Foundation
+
+/*
+ 226. 翻转二叉树 (简单)
+ https://leetcode-cn.com/problems/invert-binary-tree/
+ 
+ 解法：
+ 1. 递归
+ 2. BFS
+ 3. DFS
+ */
+
+class Solution_226 {
+    
+    /// DFS
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        if root == nil {
+            return nil
+        }
+        
+        var stack = [root!]
+        while !stack.isEmpty {
+            let node = stack.removeLast()
+            let tmp = node.left
+            node.left = node.right
+            node.right = tmp
+            
+            if node.left != nil {
+                stack.append(node.left!)
+            }
+            if node.right != nil {
+                stack.append(node.right!)
+            }
+        }
+        
+        return root
+    }
+    
+    /// BFS (层次遍历翻转)
+    func invertTree_2(_ root: TreeNode?) -> TreeNode? {
+        if root == nil {
+            return nil
+        }
+        
+        var queue = [root!]
+        while !queue.isEmpty {
+            let node = queue.removeFirst()
+            let tmp = node.left
+            node.left = node.right
+            node.right = tmp
+            
+            if node.left != nil {
+                queue.append(node.left!)
+            }
+            if node.right != nil {
+                queue.append(node.right!)
+            }
+        }
+        
+        return root
+    }
+    
+    /// 递归
+    func invertTree_1(_ root: TreeNode?) -> TreeNode? {
+        if root == nil {
+            return nil
+        }
+        
+        let left = invertTree(root?.left)
+        let right = invertTree(root?.right)
+        
+        root?.left = right
+        root?.right = left
+        
+        return root
+    }
+    
+    func test() {
+        let root = createTreeNode([4,2,7,1,3,6,9])
+        print(invertTree(root)!.levelOrder())
+    }
+}
