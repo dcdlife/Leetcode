5d5b707ed818b5da3ba6b61ea025bf5723341949
devchongzai <71340227+devchongzai@users.noreply.github.com>
2022-04-15 06:11:52 +0800

    更新注释 面试题 04.06. 后继者 (中等)

--- Leetcode-Swift/程序员面试金典(第6版)/04.06.swift
+++ Leetcode-Swift/程序员面试金典(第6版)/04.06.swift
@@ -48,12 +48,15 @@ import Foundation
          }
 
          // 来到此处 node == p，寻找node的“下一个”节点
+
+         // 如果node的右子树不为空，那么下一个节点就是右子树的最深左子节点
          if (node.right != null) {
              ans = node.right
              while (ans.left != null) {
                  ans = ans.left
              }
          } else {
+             // 来到这里，说明node的右子树为空，下一个节点是栈顶节点
              if (stack.length > 0) {
                  ans = stack[stack.length - 1]
              } else {
