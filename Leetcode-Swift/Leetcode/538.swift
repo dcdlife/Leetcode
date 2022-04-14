d1b6e2231d5b90dee00f8c74c85eaaede9f3c94c
devchongzai <71340227+devchongzai@users.noreply.github.com>
2022-04-15 05:43:13 +0800

    538. 把二叉搜索树转换为累加树 (中等)

---
+++ Leetcode-Swift/Leetcode/538.swift
@@ -0,0 +1,40 @@
+//
+//  538.swift
+//  Leetcode-Swift
+//
+//  Created by yefeng on 2022/4/15.
+//
+
+import Foundation
+
+/*
+ 538. 把二叉搜索树转换为累加树 (中等)
+ https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+ */
+
+class Solution_538 {
+    func convertBST(_ root: TreeNode?) -> TreeNode? {
+        if root == nil {
+            return nil
+        }
+
+        var node = root
+        var preVal = 0
+        var stack = [TreeNode?]()
+
+        while node != nil || !stack.isEmpty {
+            while node != nil {
+                stack.append(node)
+                node = node?.right
+            }
+
+            node = stack.removeLast()
+            preVal = preVal + node!.val
+            node!.val = preVal
+
+            node = node?.left
+        }
+
+        return root
+    }
+}
