f58f7a66f6a77e8e3623f28e827a5384ddd70660
devchongzai <71340227+devchongzai@users.noreply.github.com>
2022-04-22 05:47:57 +0800

    543. 二叉树的直径 (简单)

---
+++ Leetcode-Swift/Leetcode/543.swift
@@ -0,0 +1,35 @@
+//
+//  543.swift
+//  Leetcode-Swift
+//
+//  Created by yefeng on 2022/4/22.
+//
+
+import Foundation
+
+/*
+ 543. 二叉树的直径 (简单)
+ https://leetcode-cn.com/problems/diameter-of-binary-tree/
+ */
+
+class Solution_543 {
+    func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+        var maxDiameter = 0
+        let _ = depth(root, &maxDiameter)
+
+        return maxDiameter
+    }
+
+    func depth(_ root: TreeNode?, _ maxDiameter: inout Int) -> Int {
+        if root == nil {
+            return 0
+        }
+
+        let lDepth = depth(root?.left, &maxDiameter)
+        let rDepth = depth(root?.right, &maxDiameter)
+
+        maxDiameter = max(maxDiameter, (lDepth + rDepth))
+
+        return max(lDepth, rDepth) + 1
+    }
+}
