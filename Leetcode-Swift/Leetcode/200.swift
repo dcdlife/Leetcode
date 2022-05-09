4c5448b4fbdcf15315da438716e112d72679eaf7
devcz <71340227+devcz@users.noreply.github.com>
2022-05-10 06:31:34 +0800

    更新 200. 岛屿数量 (中等)

--- Leetcode-Swift/Leetcode/200.swift
+++ Leetcode-Swift/Leetcode/200.swift
@@ -13,8 +13,47 @@ import Foundation
  */
 
 class Solution200 {
-    /// bfs
+    
+    /// dfs
+    var visited = [[Bool]]()
     func numIslands(_ grid: [[Character]]) -> Int {
+        let rows = grid.count
+        let cols = grid[0].count
+        visited = [[Bool]](repeating: [Bool](repeating: false, count: cols), count: rows)
+
+        var ans = 0
+        for i in 0..<rows {
+            for j in 0..<cols {
+                if !visited[i][j] && grid[i][j] == "1" {
+                    ans += 1
+                    dfs(grid, i, j, rows, cols)
+                }
+            }
+        }
+
+        return ans
+    }
+
+    func dfs(_ grid: [[Character]], _ row: Int, _ col: Int, _ rows: Int, _ cols: Int) {
+        visited[row][col] = true
+        let directions = [(0,-1), (0,1), (-1,0), (1,0)]
+        
+        for i in directions {
+            let newRow = row + i.0
+            let newCol = col + i.1
+
+            if newRow < 0 || newCol < 0 || newRow >= rows || newCol >= cols {
+                continue
+            }
+            if visited[newRow][newCol] || grid[newRow][newCol] == "0" {
+                continue
+            }
+            dfs(grid, newRow, newCol, rows, cols)
+        }
+    }
+    
+    /// bfs
+    func numIslands_bfs(_ grid: [[Character]]) -> Int {
         if grid.count == 0 || grid[0].count == 0 {
             return 0
         }
@@ -49,43 +88,6 @@ class Solution200 {
                 }
             }
         }
-        return ans
-    }
-    
-    func numIslands_dfs(_ grid: [[Character]]) -> Int {
-        if grid.count == 0 || grid[0].count == 0 {
-            return 0
-        }
-        let rows = grid.count
-        let cols = grid[0].count
-        var visited: [[Bool]] = [[Bool]](repeating: [Bool](repeating: false, count: cols), count: rows)
-        let dx = [0,0,-1,1]
-        let dy = [1,-1,0,0]
-        func _dfs(_ row: Int, _ col: Int) {
-            if row < 0 || row >= rows  || col < 0 || col >= cols || visited[row][col] {
-                return
-            }
-            
-            visited[row][col] = true
-            
-            if grid[row][col] == "0" {
-                return
-            }
-            
-            for i in 0..<4 {
-                _dfs(row + dx[i], col + dy[i]);
-            }
-        }
-        var ans = 0
-        for row in 0..<rows {
-            for col in 0..<cols {
-                if !visited[row][col] && grid[row][col] == "1" {
-                    ans += 1
-                    _dfs(row, col)
-                }
-            }
-        }
-        
         return ans
     }
     
