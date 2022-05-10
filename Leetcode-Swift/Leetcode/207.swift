4f7bf65fd28558073b3d57aed584298cf5a0500e
devcz <71340227+devcz@users.noreply.github.com>
2022-05-11 07:13:28 +0800

    207. 课程表 (中等)

--- Leetcode-Swift/Leetcode/207.swift
+++ Leetcode-Swift/Leetcode/207.swift
@@ -13,12 +13,49 @@ import Foundation
  */
 
 class Solution_207 {
-    func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
-
-        return false
-    }
+    var edges = [[Int]]()
+    var visited = [Int]()
+    var isValid = true
     
-    func test() {
-        print("test")
+    func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
+        if prerequisites.isEmpty {
+            return true
+        }
+
+        edges = [[Int]](repeating: [Int](), count: numCourses)
+        visited = [Int](repeating: 0, count: numCourses)
+
+        for i in prerequisites  {
+            edges[i[0]].append(i[1])
+        }
+
+        for i in 0..<numCourses {
+            if !isValid {
+                break
+            }
+            if visited[i] == 0 {
+                dfs(i)
+            }
+        }
+
+        return isValid
+    }
+
+    func dfs(_ index: Int) {
+        visited[index] = 1
+
+        for i in edges[index] {
+            if visited[i] == 0 {
+                dfs(i)
+                if !isValid {
+                    return
+                }
+            } else if (visited[i] == 1) {
+                isValid = false
+                return
+            }
+        }
+
+        visited[index] = 2
     }
 }
