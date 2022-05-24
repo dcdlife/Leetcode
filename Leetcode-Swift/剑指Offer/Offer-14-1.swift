73615d4c978e3b39d4d63440032c933c271942fa
devcz <71340227+devcz@users.noreply.github.com>
2022-05-25 07:43:45 +0800

    【更新】剑指 Offer 14- I. 剪绳子 (中等)

--- Leetcode-Swift/剑指Offer/Offer-14-1.swift
+++ Leetcode-Swift/剑指Offer/Offer-14-1.swift
@@ -23,13 +23,14 @@ class Solution_Offer_14_1 {
         if n == 3 {
             return 2
         }
+        
         var dp = [Int](repeating: 0, count: n + 1)
         dp[0] = 0
         dp[1] = 1
         dp[2] = 2
         dp[3] = 3
-        var maxProduct = 0
         for i in 4...n {
+            var maxProduct = 0
             for j in 1...(i / 2) {
                 maxProduct = max(maxProduct, dp[j] * dp[i - j])
                 dp[i] = maxProduct
