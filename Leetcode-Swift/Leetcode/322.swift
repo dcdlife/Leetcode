80b0328b9ac3d8985756fbd0028d73b6cc89ee41
devcz <71340227+devcz@users.noreply.github.com>
2022-05-24 09:00:05 +0800

    【更新】322. 零钱兑换 (中等)

--- Leetcode-Swift/Leetcode/322.swift
+++ Leetcode-Swift/Leetcode/322.swift
@@ -12,9 +12,8 @@ import Foundation
  https://leetcode-cn.com/problems/coin-change/
  */
 
-class Solution322 {
-
-    /// 动态规划1（优化）
+class Solution_322 {
+    /// 动态规划
     func coinChange(_ coins: [Int], _ amount: Int) -> Int {
         if amount == 0 {
             return 0
@@ -28,77 +27,13 @@ class Solution322 {
         
         for i in 1...amount {
             for coin in coins {
-                if coin <= i {
+                if i >= coin {
                     dp[i] = min(dp[i], dp[i - coin] + 1)
                 }
             }
         }
 
         return dp[amount] > amount ? -1 : dp[amount]
-    }
-    
-    /// 动态规划2（自写）
-    func coinChange_dp2(_ coins: [Int], _ amount: Int) -> Int {
-        if amount == 0 {
-            return 0
-        }
-        if coins.isEmpty || amount < 0 {
-            return -1
-        }
-
-        var dp = [Int](repeating: -1, count: amount + 1)
-        dp[0] = 0
-
-        for i in 1...amount {
-            var minCoins = Int.max
-            for coin in coins {
-                let index = i - coin
-                if (index >= 0) && dp[index] != -1 {
-                    minCoins = min(minCoins, dp[index])
-                }
-            }
-
-            if minCoins != Int.max {
-                dp[i] = minCoins + 1
-            }
-        }
-
-        return dp[amount]
-    }
-    
-    /// dfs+剪枝
-    func coinChange_dfs(_ coins: [Int], _ amount: Int) -> Int {
-        if amount == 0 {
-            return 0
-        }
-        if amount <= 0 {
-            return -1
-        }
-        
-        func inner(_ coins: [Int], _ remain: Int, _ count: inout [Int]) -> Int {
-            if remain < 0 {
-                return -1
-            }
-            if remain == 0 {
-                return 0
-            }
-            if count[remain - 1] != 0 {
-                return count[remain - 1]
-            }
-            var min = Int.max
-            for coin in coins {
-                let res = inner(coins, remain - coin, &count)
-                if res >= 0 && res < min {
-                    min = res + 1
-                }
-            }
-            count[remain - 1] = min != Int.max ? min : -1
-            return count[remain - 1]
-        }
-        
-        var count = [Int](repeating: 0, count: amount)
-        let _ = inner(coins, amount, &count)
-        return count[amount - 1]
     }
     
     func test() {
