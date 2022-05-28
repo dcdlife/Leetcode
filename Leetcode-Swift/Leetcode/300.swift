943ff732fe868b40302dfac4dcd19ee307c22b29
devcz <71340227+devcz@users.noreply.github.com>
2022-05-28 16:34:09 +0800

    【更新】300. 最长递增子序列 (中等)

--- Leetcode-Swift/Leetcode/300.swift
+++ Leetcode-Swift/Leetcode/300.swift
@@ -12,50 +12,26 @@ import Foundation
  https://leetcode-cn.com/problems/longest-increasing-subsequence/
  */
 
-class Solution300 {
+class Solution_300 {
     func lengthOfLIS(_ nums: [Int]) -> Int {
-        if nums.count == 0 {
+        if nums.isEmpty {
             return 0
         }
-        var counts = [Int](repeating: 1, count: nums.count)
-        for i in 1..<nums.count {
-            var ascendCount = 1
-            var left = 0, right = i - 1
-            while left <= right {
-                let mid = left + (right - left) / 2
-                if nums[mid] < nums[i] {
-                    ascendCount = max(counts[mid] + 1, ascendCount)
-                    left = mid + 1
-                } else {
-                    left += 1
+
+        let count = nums.count
+        var dp = [Int](repeating: 1, count: count)
+        var ans = 1
+
+        for i in 1..<count {
+            let curNumber = nums[i]
+            for j in 0..<i {
+                if nums[j] < curNumber {
+                    dp[i] = max(dp[i], dp[j] + 1)
                 }
             }
-            counts[i] = ascendCount
+            ans = max(ans, dp[i])
         }
-        return counts.max()!
-    }
-    
-    /// DP：两次遍历  O(n^2)
-    func lengthOfLIS_2iteration(_ nums: [Int]) -> Int {
-            if nums.count == 0 {
-                return 0
-            }
-            var counts = [Int](repeating: 1, count: nums.count)
-            for i in 1..<nums.count {
-                var ascendCount = 1
-                for j in 0...i-1 {
-                    if nums[j] < nums[i] {
-                        ascendCount = max(counts[j] + 1, ascendCount)
-                    }
-                }
-                counts[i] = ascendCount
-            }
-            return counts.max()!
-        }
-    
-    func test() {
-        print(lengthOfLIS([10,9,2,5,3,7,101,18]))
-        print(lengthOfLIS([0,1,0,3,2,3]))
-        print(lengthOfLIS([7,7,7]))
+
+        return ans
     }
 }
