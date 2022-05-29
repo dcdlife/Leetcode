7e1652e054a6d86f56c852e605f97411148b3929
devcz <71340227+devcz@users.noreply.github.com>
2022-05-29 08:00:34 +0800

    【新增】面试题 16.24. 数对和 (中等)

---
+++ Leetcode-Swift/程序员面试金典(第6版)/16.24.swift
@@ -0,0 +1,44 @@
+//
+//  16.24.swift
+//  Leetcode-Swift
+//
+//  Created by devcz on 2022/5/29.
+//
+
+import Foundation
+
+/*
+ 面试题 16.24. 数对和 (中等)
+ https://leetcode.cn/problems/pairs-with-sum-lcci/
+ */
+
+class Solution_16_24 {
+    func pairSums(_ nums: [Int], _ target: Int) -> [[Int]] {
+        if nums.isEmpty {
+            return []
+        }
+
+        let nums = nums.sorted()
+        var left = 0
+        var right = nums.count - 1
+        var ans = [[Int]]()
+
+        while left < right {
+            let leftVal = nums[left]
+            let rightVal = nums[right]
+            let sum = leftVal + rightVal
+
+            if sum == target {
+                ans.append([leftVal, rightVal])
+                left += 1
+                right -= 1
+            } else if sum < target {
+                left += 1
+            } else if sum > target {
+                right -= 1
+            }
+        }
+
+        return ans
+    }
+}
