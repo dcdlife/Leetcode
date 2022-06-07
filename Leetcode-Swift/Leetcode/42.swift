58985d8be1e0c4319a6a711a147c0b7959f5dbb9
devcz <71340227+devcz@users.noreply.github.com>
2022-06-08 07:54:01 +0800

    【新增】42. 接雨水 (困难)

--- Leetcode-Swift/Leetcode/42.swift
+++ Leetcode-Swift/Leetcode/42.swift
@@ -14,7 +14,28 @@ import Foundation
 
 class Solution_42 {
     func trap(_ height: [Int]) -> Int {
+        if height.isEmpty {
+            return 0
+        }
 
-        return 0
+        let count = height.count
+        var leftMaxHeight = [Int](repeating: 0, count: count)
+        var ans = 0
+        
+        var pre = height[0]
+        for i in 1..<count {
+            leftMaxHeight[i] = pre
+            pre = max(pre, height[i])
+        }
+
+        pre = height[count - 1]
+        for i in (0..<(count - 1)).reversed() {
+            let minHeight = min(leftMaxHeight[i], pre)
+            pre = max(pre, height[i])
+
+            ans += (minHeight > height[i] ? (minHeight - height[i]) : 0)
+        }
+
+        return ans
     }
 }
