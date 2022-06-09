3c905665d05a528a75afebe698f11f607315eaa8
devcz <71340227+devcz@users.noreply.github.com>
2022-06-09 08:12:14 +0800

    【新增】面试题 05.07. 配对交换 (简单)

---
+++ Leetcode-Swift/程序员面试金典(第6版)/05.07.swift
@@ -0,0 +1,45 @@
+//
+//  05.07.swift
+//  Leetcode-Swift
+//
+//  Created by devcz on 2022/6/9.
+//
+
+import Foundation
+
+/*
+ 面试题 05.07. 配对交换 (简单)
+ https://leetcode.cn/problems/exchange-lcci/
+ */
+
+class Solution_05_07 {
+    func exchangeBits(_ num: Int) -> Int {
+        var num = num
+        var count = 0
+        var odd = 1
+        var even = 1 << 1
+
+        while count < 16 {
+            var oddVal = num & odd
+            var evenVal = num & even
+
+            if oddVal == 0 {
+                num &= (~even)
+            } else {
+                num |= (even)
+            }
+
+            if evenVal == 0 {
+                num &= (~odd)
+            } else {
+                num |= (odd)
+            }
+
+            count += 1
+            odd <<= 2
+            even <<= 2
+        }
+
+        return num
+    }
+}
