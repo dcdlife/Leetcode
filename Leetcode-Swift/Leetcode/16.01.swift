1d06ab70d34d61806c91e34456b64996179b63ed
devcz <71340227+devcz@users.noreply.github.com>
2022-06-11 08:44:58 +0800

    【新增】面试题 16.01. 交换数字 (中等)

---
+++ Leetcode-Swift/Leetcode/16.01.swift
@@ -0,0 +1,28 @@
+//
+//  16.01.swift
+//  Leetcode-Swift
+//
+//  Created by devcz on 2022/6/11.
+//
+
+import Foundation
+
+/*
+ 面试题 16.01. 交换数字 (中等)
+ https://leetcode.cn/problems/swap-numbers-lcci/
+ */
+
+class Solution_16_01 {
+    func swapNumbers(_ numbers: [Int]) -> [Int] {
+        if numbers.count < 2 {
+            return []
+        }
+
+        var numbers = numbers
+        numbers[0] = numbers[0] ^ numbers[1]
+        numbers[1] = numbers[0] ^ numbers[1]
+        numbers[0] = numbers[0] ^ numbers[1]
+
+        return numbers
+    }
+}
