27a5d922a137c3d8ab394a1e457d234ca01d3ebd
devcz <71340227+devcz@users.noreply.github.com>
2022-06-17 06:28:54 +0800

    【更新】面试题 17.11. 单词距离 (中等)

--- Leetcode-Swift/程序员面试金典(第6版)/17.11.swift
+++ Leetcode-Swift/程序员面试金典(第6版)/17.11.swift
@@ -13,6 +13,7 @@ import Foundation
  */
 
 class Solution_17_11 {
+    // MARK: - 一次遍历
     func findClosest(_ words: [String], _ word1: String, _ word2: String) -> Int {
         if words.isEmpty {
             return -1
@@ -31,6 +32,45 @@ class Solution_17_11 {
 
             if index1 != -1 && index2 != -1 {
                 ans = min(ans, abs(index1 - index2))
+            }
+        }
+
+        return ans
+    }
+    
+    // MARK: - 双指针
+    func findClosest_v2(_ words: [String], _ word1: String, _ word2: String) -> Int {
+        if words.isEmpty {
+            return Int.max
+        }
+
+        var indexesOfWord1 = [Int]()
+        var indexesOfWord2 = [Int]()
+        for (index, word) in words.enumerated() {
+            if word == word1 {
+                indexesOfWord1.append(index)
+            } else if (word == word2) {
+                indexesOfWord2.append(index)
+            }
+        }
+
+        if indexesOfWord1.isEmpty || indexesOfWord2.isEmpty {
+            return Int.max
+        }
+
+        var ans = Int.max
+        var p0 = 0, p1 = 0
+
+        while p0 < indexesOfWord1.count && p1 < indexesOfWord2.count {
+            let index0 = indexesOfWord1[p0]
+            let index1 = indexesOfWord2[p1]
+
+            ans = min(ans, abs(index0 - index1))
+
+            if index0 >= index1 {
+                p1 += 1
+            } else {
+                p0 += 1
             }
         }
 
