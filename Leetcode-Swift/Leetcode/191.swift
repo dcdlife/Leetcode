0a66382bc1479bb6e31b4b7525b6fc4c7832669e
devcz <71340227+devcz@users.noreply.github.com>
2022-06-24 08:51:29 +0800

    191. 位1的个数 (简单)

--- Leetcode-Swift/Leetcode/191.swift
+++ Leetcode-Swift/Leetcode/191.swift
@@ -13,7 +13,7 @@ import Foundation
  */
 
 class Solution191 {
-    /// x & (x - 1)可以去掉最右侧的1
+    // MARK: - x & (x - 1)可以去掉最右侧的1（无法针对负数做处理）
     func hammingWeight(_ n: Int) -> Int {
         var ans = 0
         var n = n
@@ -22,6 +22,23 @@ class Solution191 {
             n &= (n - 1)
             ans += 1
         }
+        
+        return ans
+    }
+    
+    // MARK: - 考虑负数
+    func hammingWeight_v2(_ n: Int) -> Int {
+        var ans = 0
+        var mask = 1
+        
+        for _ in 0..<32 {
+            if n & mask != 0 {
+                ans += 1
+            }
+            
+            mask <<= 1
+        }
+        
         
         return ans
     }
