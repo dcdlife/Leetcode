15a223970f06d6188be1cac0e90c64401f795e32
devcz <71340227+devcz@users.noreply.github.com>
2022-06-26 08:01:09 +0800

    【更新】231. 2的幂 (简单)

--- Leetcode-Swift/Leetcode/231.swift
+++ Leetcode-Swift/Leetcode/231.swift
@@ -13,7 +13,13 @@ import Foundation
  */
 
 class Solution231 {
+    // MARK: - 二进制表示
     func isPowerOfTwo(_ n: Int) -> Bool {
         return (n > 0) && (n & (n - 1) == 0)
+    }
+    
+    // MARK: - 判断是否为最大 22 的幂的约数
+    func isPowerOfTwo_2(_ n: Int) -> Bool {
+        return (n > 0) && ((1 << 30) % n == 0)
     }
 }
