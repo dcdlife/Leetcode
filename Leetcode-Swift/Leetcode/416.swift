ad6f778ddf50e56ea6c7c3e62b51e6d61647514c
devcz <71340227+devcz@users.noreply.github.com>
2022-06-28 07:43:32 +0800

    【更新】416. 分割等和子集 (中等)

--- Leetcode-Swift/Leetcode/416.swift
+++ Leetcode-Swift/Leetcode/416.swift
@@ -13,36 +13,28 @@ import Foundation
  */
 
 class Solution_416 {
-    // MARK: - 动态规划解法
+    // MARK: - 0-1背包问题，是否能装满背包
     func canPartition(_ nums: [Int]) -> Bool {
-        if nums.isEmpty || nums.count < 2 {
+        if nums.count < 2 {
             return false
         }
 
-        var maxNum = 0
-        let sum = nums.reduce(0) {pre, i in
-            maxNum = max(i, maxNum)
-            return pre + i
-        }
+        let sum = nums.reduce(0, +)
         if sum & 1 == 1 {
             return false
         }
 
         let target = sum / 2
-        if maxNum > target {
-            return false
+        var dp = [[Bool]](repeating: [Bool](repeating: false, count: target + 1),
+                              count: nums.count)
+        dp[0][0] = true
+        if nums[0] <= target {
+            dp[0][nums[0]] = true
         }
-
-        var dp = [[Bool]](repeating: [Bool](repeating: false, count: target + 1), count: nums.count)
-
-        for i in 0..<nums.count {
-            dp[i][0] = true
-        }
-        dp[0][nums[0]] = true
 
         for i in 1..<nums.count {
             let num = nums[i]
-            for j in 1...target {
+            for j in 0...target {
                 if j < num {
                     dp[i][j] = dp[i - 1][j]
                 } else {
@@ -52,5 +44,35 @@ class Solution_416 {
         }
 
         return dp[nums.count - 1][target]
+    }
+    
+    // MARK: - 0-1背包问题，是否能装满背包（优化）
+    func canPartition_v2(_ nums: [Int]) -> Bool {
+        if nums.count < 2 {
+            return false
+        }
+
+        var sum = nums.reduce(0, +)
+        if sum & 1 == 1 {
+            return false
+        }
+        
+        sum /= 2
+        
+        var dp = [Bool](repeating: false, count: sum + 1)
+        dp[0] = true
+
+        for num in nums {
+            if (dp.count >= num) {
+                for i in (num..<dp.count).reversed() {
+                    dp[i] = dp[i] || dp[i - num]
+                    if i == sum && dp[sum] {
+                        return true
+                    }
+                }
+            }
+        }
+
+        return false
     }
 }
