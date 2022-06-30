eb29b11cd35450eb2b57b26037b52b30b5f980b1
devcz <71340227+devcz@users.noreply.github.com>
2022-07-01 06:26:52 +0800

    【更新】518. 零钱兑换 II (中等)

--- Leetcode-Swift/Leetcode/518.swift
+++ Leetcode-Swift/Leetcode/518.swift
@@ -13,43 +13,33 @@ import Foundation
  */
 
 class Solution_518 {
+    // MARK: - 完全背包问题 -> 计数问题（填满有多少种方法）
     func change(_ amount: Int, _ coins: [Int]) -> Int {
         if amount == 0 {
             return 1
         }
-        if coins.isEmpty || amount < 0 {
+        if amount < 0 || coins.isEmpty {
             return 0
         }
 
-        var dp = [[Int]](repeating: [Int](repeating: 0, count: amount + 1), count: coins.count)
-        for i in 0..<coins.count {
-            var val = coins[i]
-            while val <= amount {
-                dp[i][val] = 1
-                val += coins[i]
-            }
+        let count = coins.count
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: amount + 1), count: count)
+
+        var k = amount / coins[0]
+        for i in 0...k {
+            dp[0][i * coins[0]] = 1
         }
 
-        for i in 1..<coins.count {
-            let coinNumber = coins[i]
-            for j in 1...amount {
-                for k in 0..<i {
-                    if dp[k][j] != 0 {
-                        var val = j + coinNumber
-                        while val <= amount {
-                            dp[i][val] += dp[k][j]
-                            val += coinNumber
-                        }
-                    }
+        for i in 1..<count {
+            let coin = coins[i]
+            for j in 0...amount {
+                var k = j / coin
+                for c in 0...k {
+                    dp[i][j] += dp[i - 1][j - c * coin]
                 }
             }
         }
 
-        var ans = 0
-        for i in 0..<coins.count {
-            ans += dp[i][amount]
-        }
-
-        return ans
+        return dp[count - 1][amount]
     }
 }
