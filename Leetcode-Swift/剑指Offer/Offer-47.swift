6608a2744385feb1be44bd1204c5affdfb58baf1
95cc <71340227+95cc@users.noreply.github.com>
2022-07-03 09:08:53 +0800

    【更新】剑指 Offer 47. 礼物的最大价值 (中等)

--- Leetcode-Swift/剑指Offer/Offer-47.swift
+++ Leetcode-Swift/剑指Offer/Offer-47.swift
@@ -13,47 +13,49 @@ import Foundation
  */
 
 class Solution_Offer_47 {
+    // MARK: - 动态规划
     func maxValue(_ grid: [[Int]]) -> Int {
         if grid.isEmpty || grid[0].isEmpty {
             return 0
         }
 
-        var grid = grid
         let rows = grid.count
         let cols = grid[0].count
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: cols), count: rows)
+        dp[0][0] = grid[0][0]
 
         for row in 1..<rows {
-            grid[row][0] = grid[row][0] + grid[row - 1][0]
+            dp[row][0] = grid[row][0] + dp[row - 1][0]
         }
         for col in 1..<cols {
-            grid[0][col] = grid[0][col] + grid[0][col - 1]
+            dp[0][col] = grid[0][col] + dp[0][col - 1]
         }
 
         for i in 1..<rows {
             for j in 1..<cols {
-                grid[i][j] = grid[i][j] + max(grid[i - 1][j], grid[i][j - 1])
+                dp[i][j] = grid[i][j] + max(dp[i - 1][j], dp[i][j - 1])
             }
         }
 
-        return grid[rows - 1][cols - 1]
+        return dp[rows - 1][cols - 1]
     }
     
-    /*
-     测试用例：
-     1. 功能测试：只有1个数字；1行；1列；多行多列
-     2. 特殊输入测试：空数组
-     */
-    func test() {
-        print(maxValue([[1]]))
-        print(maxValue([[1,2,3,4,5]]))
-        print(maxValue([[1],[2],[3]]))
-        print(maxValue([
-            [1,3,1],
-            [1,5,1],
-            [4,2,1]
-          ]
-        ))
-        print(maxValue([]))
-        print(maxValue([[]]))
+    // MARK: - 动态规划（优化）
+    func maxValue_v2(_ grid: [[Int]]) -> Int {
+        if grid.isEmpty || grid[0].isEmpty {
+            return 0
+        }
+
+        let rows = grid.count
+        let cols = grid[0].count
+        var dp = [Int](repeating: 0, count: cols + 1)
+        
+        for i in 0...(rows - 1) {
+            for j in 1...cols {
+                dp[j] = grid[i][j - 1] + max(dp[j - 1], dp[j])
+            }
+        }
+
+        return dp[cols]
     }
 }
