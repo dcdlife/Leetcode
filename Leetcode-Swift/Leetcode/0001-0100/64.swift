da0822c7654b96c70249d199476384960237077b
95cc <71340227+95cc@users.noreply.github.com>
2022-07-03 08:56:30 +0800

    【更新】64. 最小路径和 (中等)

--- Leetcode-Swift/Leetcode/0001-0100/64.swift
+++ Leetcode-Swift/Leetcode/0001-0100/64.swift
@@ -13,28 +13,30 @@ import Foundation
  */
 
 class Solution_64 {
+    // MARK: - 动态规划
     func minPathSum(_ grid: [[Int]]) -> Int {
         if grid.isEmpty || grid[0].isEmpty {
             return 0
         }
 
-        var grid = grid
         let rows = grid.count
         let cols = grid[0].count
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: cols), count: rows)
+        dp[0][0] = grid[0][0]
 
         for row in 1..<rows {
-            grid[row][0] = grid[row][0] + grid[row - 1][0]
+            dp[row][0] = grid[row][0] + dp[row - 1][0]
         }
         for col in 1..<cols {
-            grid[0][col] = grid[0][col] + grid[0][col - 1]
+            dp[0][col] = grid[0][col] + dp[0][col - 1]
         }
 
         for i in 1..<rows {
             for j in 1..<cols {
-                grid[i][j] = grid[i][j] + min(grid[i - 1][j], grid[i][j - 1])
+                dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1])
             }
         }
 
-        return grid[rows - 1][cols - 1]
+        return dp[rows - 1][cols - 1]
     }
 }
