eb195dd781afdce3fe20352b81ecf5807c5383c7
95cc <71340227+95cc@users.noreply.github.com>
2022-07-05 06:19:49 +0800

    【更新】198. 打家劫舍 (中等)

--- Leetcode-Swift/Leetcode/0101-0200/198.swift
+++ Leetcode-Swift/Leetcode/0101-0200/198.swift
@@ -13,7 +13,28 @@ import Foundation
  */
 
 class Solution_198 {
+    // MARK: - 动态规划
     func rob(_ nums: [Int]) -> Int {
+        if nums.isEmpty {
+            return 0
+        }
+        if nums.count == 1 {
+            return nums[0]
+        }
+
+        var dp = [Int](repeating: 0, count: nums.count)
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
+
+        for i in 2..<nums.count {
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+        }
+
+        return dp[nums.count - 1]
+    }
+
+    // MARK: - 动态规划 (优化) (滚动数组思路)
+    func rob_v2(_ nums: [Int]) -> Int {
         if nums.isEmpty {
             return 0
         }
