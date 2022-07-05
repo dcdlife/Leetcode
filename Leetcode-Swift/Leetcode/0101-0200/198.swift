b62caec55e967d190b74fca3a49826e7c3ebbfca
95cc <71340227+95cc@users.noreply.github.com>
2022-07-06 07:44:11 +0800

    【更新】198. 打家劫舍 (中等)

--- Leetcode-Swift/Leetcode/0101-0200/198.swift
+++ Leetcode-Swift/Leetcode/0101-0200/198.swift
@@ -22,6 +22,28 @@ class Solution_198 {
             return nums[0]
         }
 
+        let count = nums.count
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: 2), count: count)
+        dp[0][0] = 0
+        dp[0][1] = nums[0]
+
+        for i in 1..<count {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1])
+            dp[i][1] = nums[i] + dp[i - 1][0]
+        }
+
+        return max(dp[count - 1][0], dp[count - 1][1])
+    }
+    
+    // MARK: - 动态规划（空间优化①）
+    func rob_v2(_ nums: [Int]) -> Int {
+        if nums.isEmpty {
+            return 0
+        }
+        if nums.count == 1 {
+            return nums[0]
+        }
+
         var dp = [Int](repeating: 0, count: nums.count)
         dp[0] = nums[0]
         dp[1] = max(dp[0], nums[1])
@@ -33,8 +55,8 @@ class Solution_198 {
         return dp[nums.count - 1]
     }
 
-    // MARK: - 动态规划 (优化) (滚动数组思路)
-    func rob_v2(_ nums: [Int]) -> Int {
+    // MARK: - 动态规划 (空间优化②。滚动数组思路，每间房屋的最高总金额只和该房屋的前两间房屋的最高总金额相关)
+    func rob_v3(_ nums: [Int]) -> Int {
         if nums.isEmpty {
             return 0
         }
