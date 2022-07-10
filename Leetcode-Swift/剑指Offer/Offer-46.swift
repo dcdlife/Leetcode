3b8e5352b46f48a46be56bcb262bb2215d75a6b0
95cc <71340227+95cc@users.noreply.github.com>
2022-07-10 08:46:04 +0800

    【更新】剑指 Offer 46. 把数字翻译成字符串 (中等)

--- Leetcode-Swift/剑指Offer/Offer-46.swift
+++ Leetcode-Swift/剑指Offer/Offer-46.swift
@@ -13,26 +13,40 @@ import Foundation
  */
 
 class Solution_Offer_46 {
+    // MARK: - 动态规划（爬楼梯思路）
     func translateNum(_ num: Int) -> Int {
         let digits = getDigits(num)
         let count = digits.count
 
-        if count <= 0 {
-            return 0
+        if count <= 0 { return 0 }
+        if count == 1 { return 1 }
+        
+        var dp = [Int](repeating: 1, count: count + 1)
+
+        for i in 2...count {
+            dp[i] = dp[i - 1]
+            if isValid(digits[i - 2], digits[i - 1]) {
+                dp[i] += dp[i - 2]
+            }
         }
-        if count == 1 {
-            return 1
-        }
+
+        return dp[count]
+    }
+
+    // MARK: - 动态规划（利用元组）
+    func translateNum_v2(_ num: Int) -> Int {
+        let digits = getDigits(num)
+        let count = digits.count
+
+        if count <= 0 { return 0 }
+        if count == 1 { return 1 }
 
         // 元组表示：(与前一个数组合，不与前一个数组合)
         var dp = Array<(Int, Int)>.init(repeating: (0, 0), count: count + 1)
         dp[1] = (0, 1)
 
         for i in 2...count {
-            let curDigit = digits[i - 1]
-            let lastDigit = digits[i - 2]
-
-            if lastDigit == 0 || lastDigit * 10 + curDigit > 25 {
+            if isValid(digits[i - 2], digits[i - 1]) {
                 dp[i] = (0, dp[i - 1].0 + dp[i - 1].1)
             } else {
                 dp[i] = (dp[i - 1].1, dp[i - 1].0 + dp[i - 1].1)
@@ -40,6 +54,13 @@ class Solution_Offer_46 {
         }
 
         return dp.last!.0 + dp.last!.1
+    }
+
+    // MARK: -
+    func isValid(_ numA: Int, _ numB: Int) -> Bool {
+        if numA == 0 { return false }
+        if numA * 10 + numB > 25 { return false }
+        return true
     }
 
     func getDigits(_ num: Int) -> [Int] {
@@ -58,23 +79,5 @@ class Solution_Offer_46 {
         }
 
         return nums
-    }
-    
-    /*
-     测试用例：
-     1. 功能测试：只有1个数字；多个数字
-     2. 特殊输入测试：0、包含25、26的数
-     */
-    func test() {
-        print(translateNum(99999999999))
-        print(translateNum(9))
-        print(translateNum(12258))
-        print(translateNum(122581224))
-        print(translateNum(0)) // 结果应该是1
-        print(translateNum(25))
-        print(translateNum(26))
-        
-        // LeetCode未过用例
-        print(translateNum(506))
     }
 }
