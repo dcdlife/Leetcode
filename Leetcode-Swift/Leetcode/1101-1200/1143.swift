ad7b434a016ecbcc2da4b9384e813c8df1b412b3
95cc <71340227+95cc@users.noreply.github.com>
2022-07-11 06:29:43 +0800

    【更新】1143. 最长公共子序列 (中等)

--- Leetcode-Swift/Leetcode/1101-1200/1143.swift
+++ Leetcode-Swift/Leetcode/1101-1200/1143.swift
@@ -13,7 +13,35 @@ import Foundation
  */
 
 class Solution_1143 {
+    // MARK: - 动态规划
     func longestCommonSubsequence(_ text1: String, _ text2: String) -> Int {
+        if text1.isEmpty || text2.isEmpty {
+            return 0
+        }
+        if text1 == text2 {
+            return text1.count
+        }
+
+        let text1 = Array(text1)
+        let text2 = Array(text2)
+        let m = text1.count
+        let n = text2.count
+        
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: n + 1), count: m + 1)
+        for i in 1...m {
+            for j in 1...n {
+                if text1[i - 1] == text2[j - 1] {
+                    dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i - 1][j], dp[i][j - 1])
+                } else {
+                    dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
+                }
+            }
+        }
+        return dp[m][n]
+    }
+    
+    // MARK: - 动态规划（优化）
+    func longestCommonSubsequence_v2(_ text1: String, _ text2: String) -> Int {
         if text1.isEmpty || text2.isEmpty {
             return 0
         }
