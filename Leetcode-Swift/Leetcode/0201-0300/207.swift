8a2ffb80c0c3f928f5b05d287a5ff661cb415304
95cc <71340227+95cc@users.noreply.github.com>
2022-07-15 22:59:30 +0800

    【更新】207. 课程表 (中等)

--- Leetcode-Swift/Leetcode/0201-0300/207.swift
+++ Leetcode-Swift/Leetcode/0201-0300/207.swift
@@ -13,49 +13,36 @@ import Foundation
  */
 
 class Solution_207 {
-    var edges = [[Int]]()
-    var visited = [Int]()
-    var isValid = true
-    
+    // MARK: - 拓扑排序 (Kahn算法)
     func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
-        if prerequisites.isEmpty {
-            return true
+        var adjacents = [[Int]](repeating: [], count: numCourses)
+        var indegrees = [Int](repeating: 0, count: numCourses)
+
+        for i in prerequisites {
+            adjacents[i[1]].append(i[0])
+            indegrees[i[0]] += 1
         }
 
-        edges = [[Int]](repeating: [Int](), count: numCourses)
-        visited = [Int](repeating: 0, count: numCourses)
-
-        for i in prerequisites  {
-            edges[i[0]].append(i[1])
-        }
-
+        var zeroIndegrees = [Int]()
         for i in 0..<numCourses {
-            if !isValid {
-                break
-            }
-            if visited[i] == 0 {
-                dfs(i)
+            if indegrees[i] == 0 {
+                zeroIndegrees.append(i)
             }
         }
 
-        return isValid
-    }
+        var zeroIndegreeCount = 0
+        while !zeroIndegrees.isEmpty {
+            zeroIndegreeCount += 1
 
-    func dfs(_ index: Int) {
-        visited[index] = 1
-
-        for i in edges[index] {
-            if visited[i] == 0 {
-                dfs(i)
-                if !isValid {
-                    return
+            let point = zeroIndegrees.removeLast()
+            for i in adjacents[point] {
+                indegrees[i] -= 1
+                if indegrees[i] == 0 {
+                    zeroIndegrees.append(i)
                 }
-            } else if (visited[i] == 1) {
-                isValid = false
-                return
             }
         }
 
-        visited[index] = 2
+        return zeroIndegreeCount == numCourses
     }
 }
